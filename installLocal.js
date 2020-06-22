a742d1f6a8eda18a29411464814c7fde926d1853
Dustin Sanders <dustinsanders91@gmail.com>
2020-06-22 15:11:00 -0500

    initial commit

---
+++ installLocal.js
@@ -0,0 +1,15 @@
+const { execSync } = require('child_process')
+const { name, version} = require('./package.json')
+
+const vsix = `${name}-${version}.vsix`
+
+console.log('vsix', vsix)
+
+const commands = [
+  'vsce package',
+  // `code --install-extension ${vsix}`,
+]
+
+commands.map(
+  command => execSync(command, { stdio: 'inherit' })
+)
